1afd41818a59336536d6c404d98bb851ec9d2de7
Rodrigo Rocha <rodrigorgs@gmail.com>
2018-02-13 09:27:25 -0300

    Conserta exemplo de AppPreferences.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { AppPreferences } from '@ionic-native/app-preferences';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular/platform/platform';
 
 @Component({
   selector: 'page-home',
@@ -8,13 +9,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   count: number = 0;
-  constructor(public navCtrl: NavController, public prefs: AppPreferences) {
-    prefs.fetch('dict', 'count').then((res) => { this.count = Number(res); });
+
+  constructor(public navCtrl: NavController,
+      public prefs: AppPreferences,
+      public platform: Platform) {
+    this.platform.ready().then(() => this.loadCounter());
+  }
+
+  loadCounter() {
+    this.prefs.fetch(null, 'count')
+      .then((res) => { this.count = Number(res); })
   }
 
   increment() {
     this.count++;
-    this.prefs.store('dict', 'count', this.count)
+    this.prefs.store(null, 'count', this.count)
   }
-
 }
